da62d82a3ad4b74d16bf83ea27f16507c87b1faf
kimwoosung <kipsong133@gmail.com>
2021-03-23 11:14:13 +0900

    정상 merging. 0323

--- gongmanse/Model/Registration/Entity/RegistrationInput.swift
+++ gongmanse/Model/Registration/Entity/RegistrationInput.swift
@@ -27,28 +27,3 @@ struct RegistrationInput: Codable {
     var zip: Int                    // 431212
     var country: String             // Korea
 }
-//
-//  RegistrationResponse.swift
-//  gongmanse
-//
-//  Created by 김우성 on 2021/03/22.
-//
-//  https://api.gongmanse.com/v1/members (response)
-
-import Foundation
-
-
-struct RegistrationResponse: Decodable {
-    // response
-    var code: Int
-    var message: String
-    var errors: Error?
-}
-
-struct Error: Decodable {
-    var username: String?
-    var confirmPassword: String?
-    var nickname: String?
-    var phoneNumber: String?
-    var verificationCode: String?
-}
