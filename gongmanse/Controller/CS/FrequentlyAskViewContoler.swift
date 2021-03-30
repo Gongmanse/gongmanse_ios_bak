7acdec1dfb120562c83511eec710f5bcb0e7c56f
taeuk178 <dnwndlsdlsi@gmail.com>
2021-03-30 16:54:38 +0900

    Feat: SideMenu_CS UI 생성

---
+++ gongmanse/Controller/CS/FrequentlyAskViewContoler.swift
@@ -0,0 +1,21 @@
+//
+//  FrequentlyAskViewContoler.swift
+//  gongmanse
+//
+//  Created by taeuk on 2021/03/30.
+//
+
+import UIKit
+
+class FrequentlyAskViewContoler: UIViewController {
+
+    var pageIndex = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .orange
+    }
+
+    
+
+}
---
+++ gongmanse/Controller/CS/OneonOneEnquiryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OneonOneEnquiryViewController.swift
+//  gongmanse
+//
+//  Created by taeuk on 2021/03/30.
+//
+
+import UIKit
+
+class OneonOneEnquiryViewController: UIViewController {
+
+    var pageIndex = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
