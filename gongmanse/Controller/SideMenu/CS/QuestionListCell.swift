f010ab7f68cb01ba6a9da61f8a8c81fcdc7404aa
taeuk178 <dnwndlsdlsi@gmail.com>
2021-03-31 10:37:41 +0900

    Feat: SideMenu_CS_QuestionXib 생성

---
+++ gongmanse/Controller/SideMenu/CS/QuestionListCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuestionListCell.swift
+//  gongmanse
+//
+//  Created by taeuk on 2021/03/31.
+//
+
+import UIKit
+
+class QuestionListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
