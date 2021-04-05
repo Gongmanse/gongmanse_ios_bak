c9c89de0f8e17ca8f3c2f6c2acddecf1a41db7ea
taeuk178 <dnwndlsdlsi@gmail.com>
2021-04-05 10:11:05 +0900

    Feat: SideMenu_공지사항_공지사항 리스트 collectionView UI 생성

--- gongmanse/Controller/SideMenu/NoticeTab/NoticeListVC.swift
+++ gongmanse/Controller/SideMenu/NoticeTab/NoticeListVC.swift
@@ -10,21 +10,57 @@ import UIKit
 class NoticeListVC: UIViewController {
 
     var pageIndex = 0
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let NoticeIdentifier = "NoticeCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .orange
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsVerticalScrollIndicator = false
+        
+        let nibName = UINib(nibName: NoticeIdentifier, bundle: nil)
+        collectionView.register(nibName, forCellWithReuseIdentifier: NoticeIdentifier)
+        
+        let flowlayout: UICollectionViewFlowLayout
+        flowlayout = UICollectionViewFlowLayout()
+        flowlayout.sectionInset = UIEdgeInsets.zero
+        
+        flowlayout.minimumInteritemSpacing = 10
+        
+        let collectionWidth = UIScreen.main.bounds.width
+        flowlayout.itemSize = CGSize(width: collectionWidth, height: 234)
+        
+        self.collectionView.collectionViewLayout = flowlayout
     }
 
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension NoticeListVC: UICollectionViewDelegate {
+    
+}
+
+extension NoticeListVC: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NoticeIdentifier, for: indexPath) as? NoticeCell else { return UICollectionViewCell() }
+        
+        cell.contentImage.image = UIImage(named: "five")
+        cell.contentTitle.text = "AA"
+        cell.contentViewer.text = "2"
+        cell.createContentDate.text = "2017.02.12"
+        
+        return cell
+    }
+}
 
+extension NoticeListVC: UICollectionViewDelegateFlowLayout {
+    
 }
