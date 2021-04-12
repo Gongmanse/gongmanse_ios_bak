9aadb70b45aebb935541b546052f77e073f635b0
taeuk178 <dnwndlsdlsi@gmail.com>
2021-04-12 17:06:58 +0900

    Style: extension String, ImageView - 위치 수정 -> Extension 파일

--- gongmanse/Controller/SideMenu/NoticeTab/NoticeListVC.swift
+++ gongmanse/Controller/SideMenu/NoticeTab/NoticeListVC.swift
@@ -68,10 +68,10 @@ extension NoticeListVC: UICollectionViewDataSource {
         let contentImageName = noticeListArray[indexPath.row].sContent
         
         //정규식
-        let head = "(http(s?):\\/\\/file\\.gongmanse\\.com\\/uploads\\/editor\\/96\\/[a-z0-9]{0,}\\.(png|jpg))"
+        let imageRegex = "(http(s?):\\/\\/file\\.gongmanse\\.com\\/uploads\\/editor\\/96\\/[a-z0-9]{0,}\\.(png|jpg))"
         
         var allRegex: [String] = []
-        allRegex.append(contentsOf: contentImageName.getArrayAfterRegex(regex: head))
+        allRegex.append(contentsOf: contentImageName.getCertificationNumber(regex: imageRegex))
         
         print(allRegex[0])
 
@@ -95,50 +95,4 @@ extension NoticeListVC: UICollectionViewDelegateFlowLayout {
     
 }
 
-extension String{
-    func getArrayAfterRegex(regex: String) -> [String] {
-        
-        do {
-            let regex = try NSRegularExpression(pattern: regex)
-            let results = regex.matches(in: self,
-                                        range: NSRange(self.startIndex..., in: self))
-            return results.map {
-                String(self[Range($0.range, in: self)!])
-            }
-        } catch let error {
-            print("invalid regex: \(error.localizedDescription)")
-            return []
-        }
-    }
-}
 
-extension UIImageView {
-    func setImageUrl(_ url: String) {
-            
-            let cacheKey = NSString(string: url) // 캐시에 사용될 Key 값
-            
-            if let cachedImage = ImageCacheManager.shared.object(forKey: cacheKey) { // 해당 Key 에 캐시이미지가 저장되어 있으면 이미지를 사용
-                self.image = cachedImage
-                return
-            }
-            
-            DispatchQueue.global(qos: .background).async {
-                if let imageUrl = URL(string: url) {
-                    URLSession.shared.dataTask(with: imageUrl) { (data, res, err) in
-                        if let _ = err {
-                            DispatchQueue.main.async {
-                                self.image = UIImage()
-                            }
-                            return
-                        }
-                        DispatchQueue.main.async {
-                            if let data = data, let image = UIImage(data: data) {
-                                ImageCacheManager.shared.setObject(image, forKey: cacheKey) // 다운로드된 이미지를 캐시에 저장
-                                self.image = image
-                            }
-                        }
-                    }.resume()
-                }
-            }
-        }
-}
