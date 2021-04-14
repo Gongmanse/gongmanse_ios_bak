fa7e4c21fb9f094f5027949c632cac7cc651c8fc
startwo09 <startwo09@gamil.com>
2021-04-14 15:30:21 +0900

    [추가] Progress: data class (임시) ProgressFragment (메인, 국영수, 과학, 사회, 기타) ProgressAdapter (Tab, RVA, Adapter) OnBottomSheetToUnitListener (공용 필터링 리스너)
    Commons: checkCameraPermission 함수(카메라 권한)
    
    [수정]
    Intro: 클릭이벤트 Override onClick 방식

---
+++ app/src/main/java/com/gongmanse/app/data/model/progress/Header.kt
@@ -0,0 +1,7 @@
+package com.gongmanse.app.data.model.progress
+
+data class Header(
+    val isMore: String,
+    val resultMsg: String,
+    val totalRows: String
+)
---
+++ app/src/main/java/com/gongmanse/app/feature/main/progress/adapter/ProgressTabPagerAdapter.kt
@@ -0,0 +1,41 @@
+@file:Suppress("DEPRECATION")
+
+package com.gongmanse.app.feature.main.progress.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.gongmanse.app.feature.main.progress.ProgressEtcFragment
+import com.gongmanse.app.feature.main.progress.ProgressKEMFragment
+import com.gongmanse.app.feature.main.progress.ProgressScienceFragment
+import com.gongmanse.app.feature.main.progress.ProgressSocietyFragment
+import com.gongmanse.app.utils.Constants
+
+
+class ProgressTabPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val fragment = ArrayList<Fragment>()
+
+    init {
+        fragment.add(ProgressKEMFragment())
+        fragment.add(ProgressScienceFragment())
+        fragment.add(ProgressSocietyFragment())
+        fragment.add(ProgressEtcFragment())
+    }
+
+    override fun getCount() = fragment.size
+
+    override fun getItem(position: Int): Fragment {
+       return fragment[position]
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+            return when(position) {
+                0 -> Constants.Progress.TAB_TITLE_KEM
+                1 -> Constants.Progress.TAB_TITLE_SCIENCE
+                2 -> Constants.Progress.TAB_TITLE_SOCIETY
+                3 -> Constants.Progress.TAB_TITLE_ETC
+                else -> null
+            }
+    }
+}
