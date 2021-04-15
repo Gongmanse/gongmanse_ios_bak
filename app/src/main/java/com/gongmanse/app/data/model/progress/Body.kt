7d2dd303e93be69f88ce7a393d07756029c5a4ed
startwo09 <startwo09@gamil.com>
2021-04-15 11:29:36 +0900

    [추가] Progress: data class, item_progress.xml, layout_empty.xml

--- app/src/main/java/com/gongmanse/app/data/model/progress/Body.kt
+++ app/src/main/java/com/gongmanse/app/data/model/progress/Body.kt
@@ -1,5 +1,13 @@
 package com.gongmanse.app.data.model.progress
 
 data class Body(
-    var itemType : Int = 0
+    val grade: String,
+    val gradeNum: String,
+    val gradeNum2: String? = null,
+    val gradeNum3: String? = null,
+    val progressId: String,
+    val subject: String,
+    val subjectColor: String,
+    val title: String,
+    val totalCount: String
 )
--- app/src/main/java/com/gongmanse/app/data/model/progress/Progress.kt
+++ app/src/main/java/com/gongmanse/app/data/model/progress/Progress.kt
@@ -1,6 +1,6 @@
 package com.gongmanse.app.data.model.progress
 
 data class Progress(
-    val body: ArrayList<Body>,
+    val body: List<Body>,
     val header: Header
 )
