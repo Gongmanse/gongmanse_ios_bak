c910a7933e72376e952652b203a83d43b6acf0f2
kimwoosung <kipsong133@gmail.com>
2021-04-22 17:50:09 +0900

    Fix sideBar HeaderView

---
+++ gongmanse/Controller/SideMenu/SideMenuHeaderView.swift
@@ -0,0 +1,237 @@
+//
+//  SideMenuHeaderView.swift
+//  gongmanse
+//
+//  Created by 김우성 on 2021/04/22.
+//
+
+import UIKit
+
+
+protocol SideMenuHeaderViewDelegate: class {
+    func handleDismiss()
+    func clickedLoginButton()
+    func clickedRegistrationButton()
+    func clickedBuyingPassTicketButton()
+}
+
+class SideMenuHeaderView: UIView {
+    
+    // MARK: - Properties
+    
+    weak var delegate: SideMenuHeaderViewDelegate?
+    
+    /// "X" 버튼
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "close24Px"), for: .normal)
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    /// 프로필 이미지
+    let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .center
+        imageView.image = #imageLiteral(resourceName: "logoIconGray")
+        return imageView
+    }()
+    
+    /// 로그인 상태
+    let nickName: UILabel = {
+        let label = UILabel()
+        label.text = "공만세 (master)"
+        label.textColor = .mainOrange
+        label.font = UIFont.appBoldFontWith(size: 12)
+        return label
+    }()
+    
+    /// 로그인 설명
+    let membershipLevel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.appBoldFontWith(size: 12)
+        label.text = "공만세 님은 일반 회원입니다."
+        return label
+    }()
+    
+    /// 로그인 버튼
+    private let loginBtn: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = UIFont.appBoldFontWith(size: 12)
+        button.titleLabel?.textColor = UIColor.white
+        button.setTitle("로그인", for: .normal)
+        button.backgroundColor = .mainOrange
+        button.addTarget(self, action: #selector(clickedLoginButton), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    /// 회원가입 버튼
+    private let signUpBtn: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.textColor = UIColor.white
+        button.titleLabel?.font = UIFont.appBoldFontWith(size: 12)
+        button.setTitle("회원가입", for: .normal)
+        button.backgroundColor = UIColor.rgb(red: 164, green: 164, blue: 164)
+        button.addTarget(self, action: #selector(clickedRegistrationButton), for: .touchUpInside)
+        return button
+    }()
+    
+    /// 이용권 객체를 담을 View
+    let passTicketContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.rgb(red: 21, green: 176, blue: 172)
+        return view
+    }()
+    
+    /// "이용권을 구매해주세요" or "이용권 00일 남음" 레이블
+    private let buyingPassTicketLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.appBoldFontWith(size: 12)
+        label.text = "이용권을 구매해주세요."
+        return label
+    }()
+    
+    
+    /// "이용권 구매" or "사용 기간" 연장 버튼
+    private let buyingPassTicketButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.titleLabel?.font = UIFont.appRegularFontWith(size: 12)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("이용권 구매", for: .normal)
+        button.addTarget(self, action: #selector(clickedBuyingPassTicketButton), for: .touchUpInside)
+        return button
+    }()
+    
+    private let bottomBorderView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .mainOrange
+        return view
+    }()
+    
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handleDismiss() {
+        delegate?.handleDismiss()
+    }
+    
+    
+    @objc func clickedLoginButton() {
+        delegate?.clickedLoginButton()
+    }
+    
+    @objc func clickedRegistrationButton() {
+        delegate?.clickedRegistrationButton()
+    }
+    
+    @objc func clickedBuyingPassTicketButton() {
+        delegate?.clickedBuyingPassTicketButton()
+    }
+    
+    
+    // MARK: - Helpers
+    
+    func commonInit() {
+        
+        self.addSubview(closeButton)
+        self.addSubview(profileImage)
+        self.addSubview(nickName)
+        self.addSubview(membershipLevel)
+        self.addSubview(loginBtn)
+        self.addSubview(signUpBtn)
+        self.addSubview(passTicketContainerView)
+        passTicketContainerView.addSubview(buyingPassTicketLabel)
+        passTicketContainerView.addSubview(buyingPassTicketButton)
+        self.addSubview(bottomBorderView)
+        
+        closeButton.setDimensions(height: 25, width: 25)
+        closeButton.anchor(top: self.safeAreaLayoutGuide.topAnchor,
+                           right: self.safeAreaLayoutGuide.rightAnchor,
+                           paddingTop: 10,
+                           paddingRight: 10)
+        
+        
+        let profileImageConstant = self.frame.height * 0.32
+        let viewWidth = self.frame.width
+        
+        profileImage.addShadow()
+        profileImage.setDimensions(height: profileImageConstant,
+                                   width: profileImageConstant)
+        profileImage.layer.cornerRadius = profileImageConstant * 0.5
+        profileImage.backgroundColor = UIColor.rgb(red: 237, green: 237, blue: 237)
+        profileImage.centerX(inView: self)
+        profileImage.anchor(top: self.safeAreaLayoutGuide.topAnchor,
+                            paddingTop: viewWidth * 0.11)
+        
+        nickName.centerX(inView: self)
+        nickName.anchor(top: profileImage.bottomAnchor,
+                        paddingTop: viewWidth * 0.05,
+                        height: viewWidth * 0.05)
+        
+        membershipLevel.centerX(inView: self)
+        membershipLevel.anchor(top: nickName.bottomAnchor,
+                               paddingTop: viewWidth * 0.019,
+                               height: 12.5)
+        
+        loginBtn.layer.cornerRadius = 10
+        loginBtn.setDimensions(height: viewWidth * 0.097,
+                               width: viewWidth * 0.35)
+        loginBtn.anchor(top: membershipLevel.bottomAnchor,
+                        left: self.leftAnchor,
+                        paddingTop: viewWidth * 0.043,
+                        paddingLeft: viewWidth * 0.125)
+        
+        signUpBtn.layer.cornerRadius = 10
+        signUpBtn.setDimensions(height: viewWidth * 0.097,
+                                width: viewWidth * 0.35)
+        signUpBtn.anchor(top: membershipLevel.bottomAnchor,
+                         right: self.rightAnchor,
+                         paddingTop: viewWidth * 0.043,
+                         paddingRight: viewWidth * 0.125)
+        
+        passTicketContainerView.layer.cornerRadius = 10
+        passTicketContainerView.setDimensions(height: viewWidth * 0.12,
+                                              width: self.frame.width * 0.83)
+        passTicketContainerView.centerX(inView: self)
+        passTicketContainerView.anchor(top: signUpBtn.bottomAnchor,
+                                       paddingTop: viewWidth * 0.023)
+        
+        
+        buyingPassTicketLabel.centerY(inView: passTicketContainerView)
+        buyingPassTicketLabel.anchor(left: passTicketContainerView.leftAnchor,
+                                     paddingLeft: self.frame.width * 0.066,
+                                     height: 13)
+        
+        buyingPassTicketButton.layer.cornerRadius = 8.5
+        buyingPassTicketButton.setDimensions(height: self.frame.height * 0.09,
+                                             width: self.frame.width * 0.33)
+        buyingPassTicketButton.centerY(inView: passTicketContainerView)
+        buyingPassTicketButton.anchor(right: passTicketContainerView.rightAnchor,
+                                      paddingRight: 5)
+        
+        bottomBorderView.setDimensions(height: 3, width: self.frame.width)
+        bottomBorderView.centerX(inView: self)
+        bottomBorderView.anchor(bottom: self.bottomAnchor)
+    }
+    
+}
--- gongmanse/Controller/SideMenu/SideMenuVC.swift
+++ gongmanse/Controller/SideMenu/SideMenuVC.swift
@@ -2,23 +2,12 @@ import UIKit
 
 class SideMenuVC: UITableViewController {
     
-    //프로필 이미지
-    let profileImage = UIImageView()
-    //로그인 상태
-    let nickName = UILabel()
-    //로그인 설명
-    let membershipLevel = UILabel()
-    //로그인 버튼
-    let loginBtn = UIButton()
-    //회원가입 버튼
-    let signUpBtn = UIButton()
-    //닫기 버튼
-    let closeBtn = UIButton()
-    
-    //목록 데이터 배열
+    // MARK: - Properties
+
+    ///목록 데이터 배열
     let titles = ["나의 활동", "나의 일정", "공만세란?", "공지사항", "고객센터", "설정"]
     
-    //아이콘 데이터 배열
+    ///아이콘 데이터 배열
     let icons = [
         UIImage(named: "myActivity"),
         UIImage(named: "mySchedule"),
@@ -28,110 +17,38 @@ class SideMenuVC: UITableViewController {
         UIImage(named: "settings")
     ]
 
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //테이블 뷰의 헤더 역할을 할 뷰를 정의
-        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 270))
-        headerView.backgroundColor = .white
-        
-        //테이블 뷰의 헤더 뷰로 지정
-        self.tableView.tableHeaderView = headerView
-        
-        //프로필 이미지 설정
-        //self.profileImage.image = UIImage(named: "logoIconGray.png")
-        self.profileImage.frame = CGRect(x: 118, y: 37, width: 94, height: 94) //위치와 크기를 정의
-        self.profileImage.layer.cornerRadius = (self.profileImage.frame.width / 2) //반원 형태로 라운딩
-        self.profileImage.layer.borderWidth = 0 //border 두께 0 으로
-        self.profileImage.layer.masksToBounds = true //마스크 효과
-        self.profileImage.backgroundColor = #colorLiteral(red: 0.9294117647, green: 0.9294117647, blue: 0.9294117647, alpha: 1)
-        self.profileImage.layer.shadowOffset = CGSize(width: 5, height: 5)
-        self.profileImage.layer.shadowOpacity = 2.0
-        self.profileImage.layer.shadowRadius = 5
-        self.profileImage.layer.shadowColor = UIColor.gray.cgColor
-        self.profileImage.translatesAutoresizingMaskIntoConstraints = false
-        
-        //헤더 뷰에 추가
-        view.addSubview(self.profileImage)
-        
-        //로그인 상태
-        self.nickName.text = "로그인을 해주세요." //label text 내용
-        self.nickName.textColor = #colorLiteral(red: 0.9294117647, green: 0.462745098, blue: 0, alpha: 1) //label text 색상
-        self.nickName.frame = CGRect(x: 98, y: 150, width: 150, height: 30) //위치와 크기를 정의
-        self.nickName.font = UIFont.systemFont(ofSize: 18) //폰트 사이즈
-        self.nickName.backgroundColor = .clear //배경 색상
-        
-        //헤더 뷰에 추가
-        view.addSubview(self.nickName)
-        
-        //로그인 설명
-        self.membershipLevel.text = "로그인하고 더 많은 서비스를 누리세요." //label text 내용
-        self.membershipLevel.textColor = #colorLiteral(red: 0.1529411765, green: 0.1529411765, blue: 0.1529411765, alpha: 1) //label text 색상
-        self.membershipLevel.frame = CGRect(x: 68, y: 173, width: 250, height: 30) //위치와 크기를 정의
-        self.membershipLevel.font = UIFont.systemFont(ofSize: 13) //폰트 사이즈
-        self.membershipLevel.backgroundColor = .clear //배경 색상
-        
-        //헤더 뷰에 추가
-        view.addSubview(self.membershipLevel)
-        
-        //로그인 버튼
-        self.loginBtn.setTitle("로그인", for: .normal)
-        self.loginBtn.setTitleColor(.white, for: .normal)
-        self.loginBtn.backgroundColor = #colorLiteral(red: 0.9294117647, green: 0.462745098, blue: 0, alpha: 1)
-        self.loginBtn.frame = CGRect(x: 51, y: 209, width: 105, height: 35)
-        self.loginBtn.layer.cornerRadius = 10
-        self.loginBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
-        
-        view.addSubview(self.loginBtn)
-        self.loginBtn.addTarget(self, action: #selector(handleLoginButton), for: .touchUpInside)
-        
-        //회원가입 버튼
-        self.signUpBtn.setTitle("회원가입", for: .normal)
-        self.signUpBtn.setTitleColor(.white, for: .normal)
-        self.signUpBtn.backgroundColor = #colorLiteral(red: 0.6431372549, green: 0.6431372549, blue: 0.6431372549, alpha: 1)
-        self.signUpBtn.frame = CGRect(x: 173, y: 209, width: 105, height: 35)
-        self.signUpBtn.layer.cornerRadius = 10
-        self.signUpBtn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
-        self.signUpBtn.addTarget(self, action: #selector(handleRegistration), for: .touchUpInside)
-        view.addSubview(self.signUpBtn)
-        
-        //닫기 버튼
-        self.closeBtn.setImage(UIImage(named: "largeX"), for: .normal)
-        self.closeBtn.frame = CGRect(x: 270, y: 15, width: 30, height: 30)
-        self.closeBtn.backgroundColor = .clear
-        
-        view.addSubview(self.closeBtn)
-        
-        headerView.layer.addBorder([.bottom], color: #colorLiteral(red: 0.9294117647, green: 0.462745098, blue: 0, alpha: 1), width: 3.0)
-        
-        //테이블 뷰 빈칸 숨기기
-        tableView.tableFooterView = UIView()
-        
-        self.closeBtn.addTarget(self, action: #selector(closeButton), for: .touchUpInside)
+        configureUI()
     }
     
     
     // MARK: - Actions
-    
-    @objc func closeButton() {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
+
     @objc func handleLoginButton() {
         // TODO: 로그인 이후에는 이용권이 나타날 수 있도록 처리해둘 것
-//        let vc = PassTicketVC(nibName: "PassTicketVC", bundle: nil)
-        let vc = LoginVC(nibName: "LoginVC", bundle: nil)
-        self.navigationController?.pushViewController(vc, animated: true)
+        
+        
     }
     
-    
-    
-    
-    @objc func handleRegistration() {
-        let vc = RegistrationVC(nibName: "RegistrationVC", bundle: nil)
-        let nav = UINavigationController(rootViewController: vc)
-        self.navigationController?.pushViewController(vc, animated: true)
-    }
 
+    @objc func handleRegistration() {
+        
+    }
+    
+
+    // MARK: - Helpers
+    
+    func configureUI() {
+        
+        // tableView 설정
+        tableView.isScrollEnabled = false
+    }
+    
+    
+    
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -197,5 +114,42 @@ class SideMenuVC: UITableViewController {
             */
         }
     }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let frame = CGRect(x: 0, y: 0,
+                           width: tableView.frame.width,
+                           height: 230)
+        let headerView = SideMenuHeaderView(frame: frame)
+        headerView.delegate = self
+//        headerView.passTicketContainerView.anchor(bottom: headerView.bottomAnchor,
+//                                                  height: 0)
+        headerView.passTicketContainerView.isHidden = true
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 230
+    }
 }
 
+extension SideMenuVC: SideMenuHeaderViewDelegate {
+    
+    func handleDismiss() {
+        self.dismiss(animated: true)
+    }
+    
+    func clickedLoginButton() {
+        let vc = LoginVC(nibName: "LoginVC", bundle: nil)
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func clickedRegistrationButton() {
+        let vc = RegistrationVC(nibName: "RegistrationVC", bundle: nil)
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func clickedBuyingPassTicketButton() {
+        let vc = PassTicketVC(nibName: "PassTicketVC", bundle: nil)
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
