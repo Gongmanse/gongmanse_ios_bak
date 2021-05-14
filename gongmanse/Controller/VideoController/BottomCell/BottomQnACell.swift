fa0122c1cc60cbe1b5fd26b703cf4420d0dcb575
taeuk178 <dnwndlsdlsi@gmail.com>
2021-05-14 16:07:36 +0900

    Feat: VideoQnA 하단 UI구현

--- gongmanse/Controller/VideoController/BottomCell/BottomQnACell.swift
+++ gongmanse/Controller/VideoController/BottomCell/BottomQnACell.swift
@@ -4,6 +4,18 @@
 
 import Foundation
 import UIKit
+enum QnAChat {
+    case me, others
+    
+    var chatState: UITableViewCell {
+        switch self {
+        case .me:
+            return QnAMyChatCell()
+        case .others:
+            return QnAOthersChatCell()
+        }
+    }
+}
 
 class BottomQnACell: UICollectionViewCell {
     
@@ -16,13 +28,44 @@ class BottomQnACell: UICollectionViewCell {
         return label
     }()
 
-    private let chatIdentifier = "QnAMyChatCell"
+    private let myChatIdentifier = "QnAMyChatCell"
+    private let otherChatIdentifier = "QnAOthersChatCell"
     
     private let tableView: UITableView = {
-         let table = UITableView()
+        let table = UITableView()
+        table.separatorStyle = .none
         return table
     }()
     
+    private let sendText: UITextField = {
+        let text = UITextField()
+        text.backgroundColor = .lightGray
+        text.borderStyle = .roundedRect
+        text.autocorrectionType = .no
+        text.keyboardType = .default
+        text.returnKeyType = .done
+        text.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        return text
+    }()
+    
+    private let sendButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전송", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .appBoldFontWith(size: 16)
+        button.backgroundColor = .mainOrange
+        button.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return button
+    }()
+    
+    private let bottomStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.alignment = .fill
+        stack.distribution = .fillProportionally
+        stack.axis = .horizontal
+        stack.spacing = 10
+        return stack
+    }()
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
@@ -50,18 +93,36 @@ class BottomQnACell: UICollectionViewCell {
     }
     
     func configuration() {
+        
         contentView.addSubview(tableView)
+        contentView.addSubview(bottomStackView)
+        
+        bottomStackView.addArrangedSubview(sendText)
+        bottomStackView.addArrangedSubview(sendButton)
+        
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(QnAMyChatCell.self, forCellReuseIdentifier: chatIdentifier)
+        tableView.register(QnAMyChatCell.self, forCellReuseIdentifier: myChatIdentifier)
+        tableView.register(QnAOthersChatCell.self, forCellReuseIdentifier: otherChatIdentifier)
     }
     
     func constraints() {
+        
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30).isActive = true
         tableView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: sendText.topAnchor).isActive = true
+        
+        // 하단 스택뷰
+        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20).isActive = true
+        bottomStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20).isActive = true
+        bottomStackView.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
+        
+        // 전송 버튼 width == 60
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        sendButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
     }
 }
 
@@ -76,12 +137,9 @@ extension BottomQnACell: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: chatIdentifier, for: indexPath) as? QnAMyChatCell else { return UITableViewCell() }
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: myChatIdentifier, for: indexPath) as? QnAMyChatCell else { return UITableViewCell() }
         cell.textLabel?.text = "A"
         return cell
     }
-}
-
-class BottomQnAChatCell: UITableViewCell {
-    
 }
