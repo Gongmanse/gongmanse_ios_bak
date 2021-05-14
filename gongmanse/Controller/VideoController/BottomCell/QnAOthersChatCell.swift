9734599f8469cddc8db8e54d72d2ecff9918e256
taeuk178 <dnwndlsdlsi@gmail.com>
2021-05-14 12:31:14 +0900

    Feat: QnAOthersCell

---
+++ gongmanse/Controller/VideoController/BottomCell/QnAOthersChatCell.swift
@@ -0,0 +1,23 @@
+//
+//  QnAOthersChatCell.swift
+//  gongmanse
+//
+//  Created by wallter on 2021/05/14.
+//
+
+import UIKit
+
+class QnAOthersChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
