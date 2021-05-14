4576645f0693208909cac2dd4c764a263a6f4a6f
taeuk178 <dnwndlsdlsi@gmail.com>
2021-05-14 12:02:35 +0900

    Feat: Video QnA MyChat UI

--- gongmanse/Controller/VideoController/BottomCell/BottomQnACell.swift
+++ gongmanse/Controller/VideoController/BottomCell/BottomQnACell.swift
@@ -16,6 +16,12 @@ class BottomQnACell: UICollectionViewCell {
         return label
     }()
 
+    private let chatIdentifier = "QnAMyChatCell"
+    
+    private let tableView: UITableView = {
+         let table = UITableView()
+        return table
+    }()
     
     //MARK: - Lifecycle
     
@@ -26,6 +32,9 @@ class BottomQnACell: UICollectionViewCell {
         self.backgroundColor = .white
         label.centerX(inView: self)
         label.centerY(inView: self)
+        
+        configuration()
+        constraints()
     }
  
     required init?(coder: NSCoder) {
@@ -39,4 +48,40 @@ class BottomQnACell: UICollectionViewCell {
         super.awakeFromNib()
        // initialize what is needed
     }
+    
+    func configuration() {
+        contentView.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(QnAMyChatCell.self, forCellReuseIdentifier: chatIdentifier)
+    }
+    
+    func constraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+}
+
+extension BottomQnACell: UITableViewDelegate {
+    
+}
+
+extension BottomQnACell: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: chatIdentifier, for: indexPath) as? QnAMyChatCell else { return UITableViewCell() }
+        cell.textLabel?.text = "A"
+        return cell
+    }
+}
+
+class BottomQnAChatCell: UITableViewCell {
+    
 }
