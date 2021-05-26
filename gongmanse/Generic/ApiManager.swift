a8a7915a262792a3feeda9f085545b486d62f14b
taeuk178 <dnwndlsdlsi@gmail.com>
2021-05-26 15:51:36 +0900

    style: getAlmofireGeneric 주석 추가

--- gongmanse/Generic/ApiManager.swift
+++ gongmanse/Generic/ApiManager.swift
@@ -15,6 +15,12 @@ typealias resultModel<T> = (Result<T, InfoError>) -> Void
 
 
 // Get 메소드용
+/*
+ 
+ inout 용 URL은 함수 내 지역변수로 설정하길 권장합니다
+ 메모리 접근 이슈가 있네요.
+ 
+ */
 func getAlamofireGeneric<T: Codable>(url: inout String,
                                      isConvertUrl: Bool,
                                      completion: @escaping (Result<T, InfoError>) -> Void) {
