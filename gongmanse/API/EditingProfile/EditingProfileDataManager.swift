d52876c5109df2f997ec12b767e3b88b864ff6a3
kimwoosung <kipsong133@gmail.com>
2021-06-02 11:36:03 +0900

    프로필 수정 API 연동 완료 - 프로필 이미지 추가 (DB문제로 인해 이미지 파일이 업로드는 되나 URL로 변환해주진 않으나 로직은 작동함) - 프로필 비밀번호 변경 (Validation 적용) - 프로필 닉네임 변경 - 프로필 이메일 변경 (Validation 적용)

--- gongmanse/API/EditingProfile/EditingProfileDataManager.swift
+++ gongmanse/API/EditingProfile/EditingProfileDataManager.swift
@@ -30,39 +30,5 @@ class EditingProfileDataManager {
             }
     }
     
-    /// 비밀번호 변경 API메소드
-    /// - json으로 송신 시, 정상적으로 비밀번호가 변경되지 않음
-    /// - formdata로 메소드 구성 시, 정상적으로 비밀번호가 변경됨
-    func changePassword(_ parameters: ChangePasswordInput, viewController: EditingProfileController) {
-        // Controller에서 데이터 수신
-        let data = parameters
-        
-        // 받은 데이터를 AF를 통한 업로드
-        AF.upload(multipartFormData: { MultipartFormData in
-            MultipartFormData.append(Data("\(data.username)".utf8), withName: "username")
-            MultipartFormData.append(Data("\(data.password)".utf8), withName: "password")
-        }, to: "\(Constant.BASE_URL)/v/member/updatepw")
-        
-        // Result를 Response 타입에 맞게 변환
-        .responseDecodable(of: ChangePasswordResponse.self) { response in
-            
-            switch response.result {
-            case .success(let response):
-                // 연결 성공
-                if response.code == 200 {
-                    // 회원가입 성공 시, 성공메세지 전달
-                    print("DEBUG: \(#function) 성공")
-                    
-                    
-                } else {
-                    // 화원가입 실패 시, 실패메세지 전달 (실패한 항목이 많으면 그만큼 메세지 항목이 많아짐)
-                    print("DEBUG: falid changing Password")
-                }
-                
-            case .failure(let error):
-                // 연결 실패
-                print("DEBUG: failed connection \(error.localizedDescription)")
-            }
-        }
-    }
+
 }
---
+++ gongmanse/API/EditingProfile/UpdateProfileImage/UpdateProfileImageResponse.swift
@@ -0,0 +1,13 @@
+//
+//  UpdateProfileImageResponse.swift
+//  gongmanse
+//
+//  Created by 김우성 on 2021/06/02.
+//
+
+import Foundation
+
+struct UpdateProfileImageResponse: Decodable {
+    
+    var message: String
+}
