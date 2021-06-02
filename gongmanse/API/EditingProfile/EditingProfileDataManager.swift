a5877e11676a10ddb0d053b64978cb716af4c88f
kimwoosung <kipsong133@gmail.com>
2021-06-02 12:59:29 +0900

    프로필 정보조회 - 사이드메뉴 API Response 데이터를 통한 UI 업데이트 구현

--- gongmanse/API/EditingProfile/EditingProfileDataManager.swift
+++ gongmanse/API/EditingProfile/EditingProfileDataManager.swift
@@ -31,4 +31,24 @@ class EditingProfileDataManager {
     }
     
 
+    func getProfileInfoFromAPIAtSideMenu(_ parameters: EditingProfileInput, viewController: SideMenuVC) {
+        
+        let data = parameters
+        let url = "https://api.gongmanse.com/v/member/getuserinfo?token=\(data.token)"
+        
+        /// HTTP Method: GET
+        /// API 명: "01010. 프로필 정보 조회"
+        AF.request(url)
+            .responseDecodable(of: EditingProfileResponse.self) { response in
+                
+                switch response.result {
+                case .success(let response):
+                    viewController.didSuccessNetworing(response: response)
+                    
+                case .failure(let error):
+                    print("DEBUG: 프로필데이터 수신 API 통신 실패")
+                    print("DEBUG: faild connection \(error.localizedDescription)")
+                }
+            }
+    }
 }
