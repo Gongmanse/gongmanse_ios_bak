5c673089e3fba130d281be3a68074ca38f541c00
kimwoosung <kipsong133@gmail.com>
2021-06-18 20:56:47 +0900

    PIP 다시 연결

--- gongmanse/Controller/LectureTabMenuPager/Lecture_Playlist/LecturePlaylistVC.swift
+++ gongmanse/Controller/LectureTabMenuPager/Lecture_Playlist/LecturePlaylistVC.swift
@@ -485,10 +485,13 @@ extension LecturePlaylistVC: UICollectionViewDelegate, UICollectionViewDataSourc
                 NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: nil)
                 
                 
+                
                 // 싱글톤 객체에 들어간 데이터를 초기화한다.
                 
                 let videoDataManager = VideoDataManager.shared
-
+                
+                
+                
                 let pipDataManager = PIPDataManager.shared
     //            pipDataManager.currentTeacherName = nil
     //            pipDataManager.currentVideoURL = nil
