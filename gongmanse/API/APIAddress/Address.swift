86fd1c24e53bbc4407bff8cc3b6f2b5ac962587e
jocheolhui <jochulhee@grinbi.co.kr>
2021-11-15 15:57:10 +0900

    fix, dev 도메인 테스트 완료, 기존 도메인으로 변경

--- gongmanse/API/APIAddress/Address.swift
+++ gongmanse/API/APIAddress/Address.swift
@@ -2,13 +2,20 @@ import Foundation
 import UIKit
 import Alamofire
 
-let apiBaseURL = "https://apidev.gongmanse.com"
-let fileBaseURL = "https://filedev.gongmanse.com"
+/* Server API domain */
+let apiBaseURL = "https://api.gongmanse.com"
+let fileBaseURL = "https://file.gongmanse.com"
+let webBaseURL = "https://webview.gongmanse.com"
+
+// 21.10.20 settings Dev domain
+//let apiBaseURL = "https://apidev.gongmanse.com"
+//let fileBaseURL = "https://filedev.gongmanse.com"
+//let webBaseURL = "https://webviewdev.gongmanse.com"
+
 let BannerList_URL = apiBaseURL + "/v3/video/banner"
 let Recommend_Video_URL = apiBaseURL + "/v3/video/recommend"
 let Popular_Video_URL = apiBaseURL + "/v3/video/trending"
 let KoreanEnglishMath_Video_URL = apiBaseURL + "/v3/video/subject/34?"
-let webBaseURL = "https://webviewdev.gongmanse.com"
 let Science_Video_URL = apiBaseURL + "/v3/video/subject/36?"
 let SocialStudies_Video_URL = apiBaseURL + "/v3/video/subject/35?"
 let OtherSubjects_Video_URL = apiBaseURL + "/v3/video/subject/37?"
