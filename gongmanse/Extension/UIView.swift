45b04ccee40ddd34a0fae464e2f0f315f9d16336
jocheolhui <jochulhee@grinbi.co.kr>
2022-01-10 17:28:59 +0900

    fis, pad 에서 탭바 영역 타이틀이 아이콘 아래 나올 수 있도록 수정

--- gongmanse/Extension/UIView.swift
+++ gongmanse/Extension/UIView.swift
@@ -88,3 +88,14 @@ extension UIButton {
         self.setBackgroundImage(backgroundImage, for: state)
     }
 }
+
+extension UITabBar {
+    // Workaround for iOS 11's new UITabBar behavior where on iPad, the UITabBar inside
+    // the Master view controller shows the UITabBarItem icon next to the text
+    override open var traitCollection: UITraitCollection {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return UITraitCollection(horizontalSizeClass: .compact)
+        }
+        return super.traitCollection
+    }
+}
