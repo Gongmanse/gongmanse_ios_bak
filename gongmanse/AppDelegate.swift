8100b2875b986a2e8ee134db26492807ab542f75
jocheolhui <jochulhee@grinbi.co.kr>
2022-02-10 16:53:27 +0900

    fix, 디바이스 가로모드 상태로 앱 구동 시 홈화면이 가로모드로 동작하는 현상 대응 수정.

--- gongmanse/AppDelegate.swift
+++ gongmanse/AppDelegate.swift
@@ -87,7 +87,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-    var orientationLock = UIInterfaceOrientationMask.all
+    var orientationLock = UIInterfaceOrientationMask.portrait
 
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return self.orientationLock
